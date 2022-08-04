885cbb9ef4bda96c0eba27d4a925e2145fbb323a
AHK Province <81999063+sookolin@users.noreply.github.com>
2022-08-04 19:28:19 +0300

    Update spoiler.js

--- spoiler.js
+++ spoiler.js
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
 $(function() {
         $(".wandererSpoilerHead").click(function() {
             $(this).toggleClass("wandererSpoiler");
